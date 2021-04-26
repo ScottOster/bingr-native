ef2d364891f654e320e15a281792e9ba648425f6
Hasan <hasan.afridi@outlook.com>
2021-04-26 16:56:11 +0100

    log page

--- Components/Login.js
+++ Components/Login.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import { Button, View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { Button, View, Text, Image, TextInput } from 'react-native';
+import logo from '../logo.png'
 
-export const Login = ({ navigation }) => {
+
+export const Login = ({ navigation, hostName}, setCode) => {
+const [name, setName] = useState('')
+
   return (
     <View>
+      <Image source={logo} style={{ width: 50, height: 50 }} />
+      <TextInput onChangeText={(e)=> {
+        setName(e)
+       
+        }} value={hostName} placeholder={"Enter name"}></TextInput>
       <Button
-        title='HostGame'
+        title='Host Game'
+        onPress={() => {
+          
+          navigation.navigate('HostFilter');
+        }}
+      />
+  <TextInput onChangeText={()=> {}} value={hostName} placeholder={"Enter name as guest"}></TextInput>
+  <TextInput onChangeText={()=> {setCode}}  placeholder={"Enter room-code"}></TextInput>
+  <Button
+        title='Join game'
         onPress={() => {
           navigation.navigate('HostFilter');
         }}
       />
-
-      <Text>Test hellooooooo</Text>
+      
     </View>
   );
 };
