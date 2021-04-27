60f65c2000ae00e5de6bbea909b9502404efc36a
nate <nathanmasters.com@hotmail.com>
2021-04-27 15:36:31 +0000

    styling done for login and host filter

--- Components/HostFilter.js
+++ Components/HostFilter.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { Button, Switch, View, Text } from 'react-native';
+import { TouchableOpacity, StyleSheet, Button, Switch, View, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export const HostFilter = ({ navigation }) => {
-
   const [netflix, setIsNetflixEnabled] = useState(false);
   const [amazon, setIsAmazonEnabled] = useState(false);
   const [disney, setIsDisneyEnabled] = useState(false);
@@ -11,10 +11,9 @@ export const HostFilter = ({ navigation }) => {
   const [fantasy, setIsFantasyEnabled] = useState(false);
   const [horror, setIsHorrorEnabled] = useState(false);
   const [providers, setProviders] = useState([]);
-  const [genres,setGenres] = useState([])
+  const [genres, setGenres] = useState([]);
 
   const toggleSwitch = (id) => {
-    
     if (id === 8) {
       setIsNetflixEnabled((previousState) => !previousState);
     }
@@ -36,21 +35,20 @@ export const HostFilter = ({ navigation }) => {
     if (id === 27) {
       setIsHorrorEnabled((previousState) => !previousState);
     }
-    if (id === 8 ||id === 9 || id === 337) {
+    if (id === 8 || id === 9 || id === 337) {
       setProviders((prevState) => {
-      if (providers.includes(id)) {
-        const newState = [...prevState];
-        const index = newState.indexOf(id);
-        newState.splice(index, 1);
-        return newState;
-      } else {
-        const newState = [...prevState, id];
-        return newState;
-      }
-    })
-    } else{
+        if (providers.includes(id)) {
+          const newState = [...prevState];
+          const index = newState.indexOf(id);
+          newState.splice(index, 1);
+          return newState;
+        } else {
+          const newState = [...prevState, id];
+          return newState;
+        }
+      });
+    } else {
       setGenres((prevState) => {
-
         if (genres.includes(id)) {
           const newState = [...prevState];
           const index = newState.indexOf(id);
@@ -60,101 +58,180 @@ export const HostFilter = ({ navigation }) => {
           const newState = [...prevState, id];
           return newState;
         }
-      })
+      });
     }
-  
   };
-console.log(providers, genres)
+  console.log(providers, genres);
   return (
-    <View>
-      <Text>Hello Nate</Text>
-      <Text>What are you watching on?</Text>
-      <View>
-        <Switch
-          className="setProviders"
-          id=""
-          trackColor={{ false: '#767577', true: '#81b0ff' }}
-          thumbColor={netflix ? '#f5dd4b' : '#f4f3f4'}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={() => toggleSwitch(8)}
-          value={netflix}
-        />
-        <Text>Netlix</Text>
+    <View style={styles.backGround}>
+      <View style={styles.filterBox}>
+        <Text style={styles.greeting}>Hi Nate</Text>
+        <Text style={styles.questions}>What are you watching on?</Text>
+        <View style={styles.switchAndText}>
+          <Switch
+            style={styles.switch}
+            className="setProviders"
+            id=""
+            trackColor={{ false: '#767577', true: '#81b0ff' }}
+            thumbColor={netflix ? '#f5dd4b' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={() => toggleSwitch(8)}
+            value={netflix}
+          />
+          <Text style={styles.text}>Netlix</Text>
+        </View>
+        <View style={styles.switchAndText}>
+          <Switch
+            style={styles.switch}
+            className="setProviders"
+            trackColor={{ false: '#767577', true: '#81b0ff' }}
+            thumbColor={amazon ? '#f5dd4b' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={() => toggleSwitch(9)}
+            value={amazon}
+          />
+          <Text style={styles.text}>Amazon</Text>
+        </View>
+        <View style={styles.switchAndText}>
+          <Switch
+            style={styles.switch}
+            className="setProviders"
+            trackColor={{ false: '#767577', true: '#81b0ff' }}
+            thumbColor={disney ? '#f5dd4b' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={() => toggleSwitch(337)}
+            value={disney}
+          />
+          <Text style={styles.text}>Disney</Text>
+        </View>
+        <Text style={styles.questions}>What genre?</Text>
+        <View style={styles.switchAndText}>
+          <Switch
+            style={styles.switch}
+            className="setGenres"
+            trackColor={{ false: '#767577', true: '#81b0ff' }}
+            thumbColor={action ? '#f5dd4b' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={() => toggleSwitch(28)}
+            value={action}
+          />
+          <Text style={styles.text}>Action</Text>
+        </View>
+        <View style={styles.switchAndText}>
+          <Switch
+            style={styles.switch}
+            className="setGenres"
+            trackColor={{ false: '#767577', true: '#81b0ff' }}
+            thumbColor={comedy ? '#f5dd4b' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={() => toggleSwitch(35)}
+            value={comedy}
+          />
+          <Text style={styles.text}>Comedy</Text>
+        </View>
+        <View style={styles.switchAndText}>
+          <Switch
+            style={styles.switch}
+            className="setGenres"
+            trackColor={{ false: '#767577', true: '#81b0ff' }}
+            thumbColor={fantasy ? '#f5dd4b' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={() => toggleSwitch(14)}
+            value={fantasy}
+          />
+          <Text style={styles.text}>Fantasy</Text>
+        </View>
+        <View style={styles.switchAndText}>
+          <Switch
+            style={styles.switch}
+            className="setGenres"
+            trackColor={{ false: '#767577', true: '#81b0ff' }}
+            thumbColor={horror ? '#f5dd4b' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={() => toggleSwitch(27)}
+            value={horror}
+          />
+          <Text style={styles.text}>Horror</Text>
+        </View>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('HostFilter');
+          }}
+          style={styles.button}>
+          <LinearGradient
+            start={{ x: 0.0, y: 0.0 }}
+            end={{ x: 0.0, y: 0.0 }}
+            locations={[0.0, 0.74]}
+            colors={['#4ac6cd', '#49d695']}
+            style={styles.button}
+            useAngle={true}
+            angle={100}
+            angleCenter={{ x: 0.5, y: 0.5 }}>
+            <Text style={styles.buttonText}>START</Text>
+          </LinearGradient>
+        </TouchableOpacity>
       </View>
-      <View>
-        <Switch
-          className="setProviders"
-          trackColor={{ false: '#767577', true: '#81b0ff' }}
-          thumbColor={amazon ? '#f5dd4b' : '#f4f3f4'}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={() => toggleSwitch(9)}
-          value={amazon}
-        />
-        <Text>Amazon</Text>
-      </View>
-      <View>
-        <Switch
-          className="setProviders"
-          trackColor={{ false: '#767577', true: '#81b0ff' }}
-          thumbColor={disney ? '#f5dd4b' : '#f4f3f4'}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={() => toggleSwitch(337)}
-          value={disney}
-        />
-        <Text>Disney</Text>
-      </View>
-      <Text>Select a genre</Text>
-      <View>
-        <Switch
-          className="setGenres"
-          trackColor={{ false: '#767577', true: '#81b0ff' }}
-          thumbColor={action ? '#f5dd4b' : '#f4f3f4'}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={() => toggleSwitch(28)}
-          value={action}
-        />
-        <Text>Action</Text>
-      </View>
-    <View>
-      <Switch
-          className="setGenres"
-          trackColor={{ false: '#767577', true: '#81b0ff' }}
-          thumbColor={comedy ? '#f5dd4b' : '#f4f3f4'}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={() => toggleSwitch(35)}
-          value={comedy}
-        />
-        <Text>Comedy</Text>
-    </View>
-    <View>
-      <Switch
-          className="setGenres"
-          trackColor={{ false: '#767577', true: '#81b0ff' }}
-          thumbColor={fantasy ? '#f5dd4b' : '#f4f3f4'}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={() => toggleSwitch(14)}
-          value={fantasy}
-        />
-        <Text>Fantasy</Text>
-    </View>
-    <View>
-      <Switch
-          className="setGenres"
-          trackColor={{ false: '#767577', true: '#81b0ff' }}
-          thumbColor={horror ? '#f5dd4b' : '#f4f3f4'}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={() => toggleSwitch(27)}
-          value={horror}
-        />
-        <Text>Horror</Text>
-    </View>
-    <Button
-        title='Start'
-        onPress={() => {
-          navigation.navigate('WaitingRoom');
-        }}
-      />
-
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  backGround: {
+    backgroundColor: '#f2f2f2',
+    flex: 1,
+  },
+
+  greeting: {
+    fontSize: 35,
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+    color: '#4d4d4d',
+  },
+
+  questions: {
+    fontSize: 15,
+    marginLeft: 15,
+    marginBottom: 10,
+    marginTop: 10,
+    color: '#666666',
+  },
+
+  switch: {
+    display: 'inline-block',
+    alignItem: 'center',
+    marginLeft: 50,
+    marginRight: 15,
+    margin: 7,
+  },
+
+  switchAndText: {
+    display: 'inline-block',
+    alignItem: 'center',
+  },
+
+  text: {
+    fontSize: 15,
+    color: '#666666',
+  },
+
+  button: {
+    width: 120,
+    height: 40,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginBottom: '10px',
+    marginTop: 20,
+    display: 'flex',
+    justifyContent: 'center',
+    borderRadius: 10,
+    // backgroundColor: '#2c3e50',
+  },
+
+  buttonText: {
+    fontSize: 14,
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+  },
+});
--- Components/Login.js
+++ Components/Login.js
@@ -1,57 +1,165 @@
-import React, {useState} from 'react';
-import { StyleSheet, Button, View, Text, Image, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { TouchableOpacity, StyleSheet, Button, View, Text, Image, TextInput } from 'react-native';
 import logo from '../logo.png'
 import { LinearGradient } from 'expo-linear-gradient';
 
 
-export const Login = ({ navigation, hostName}, setCode) => {
-const [name, setName] = useState('')
+export const Login = ({ navigation, hostName }, setCode) => {
+  const [name, setName] = useState('')
 
   return (
     <LinearGradient colors={['#4ac6cd', '#49d695']} style={styles.body}>
-    <View style={styles.loginPage}>
-      <Image source={logo} style={{ width: 50, height: 50 }} />
-      <TextInput onChangeText={(e)=> {
-        console.log(name);
-        setName(e)
-       
-        }} value={hostName} placeholder={"Enter name"}></TextInput>
-      <Button
-        title='Host Game'
-        onPress={() => {
-          
-          navigation.navigate('HostFilter');
-        }}
-      />
-  <TextInput onChangeText={()=> {}} value={hostName} placeholder={"Enter name as guest"}></TextInput>
-  <TextInput onChangeText={()=> {setCode}}  placeholder={"Enter room-code"}></TextInput>
-  <Button
-        title='Join game'
-        onPress={() => {
-          navigation.navigate('HostFilter');
-        }}
-      />
-      
-    </View>
+      <View style={styles.loginPage}>
+
+        <View style={styles.loginLogo}>
+          <Image style={styles.loginLogo} source={logo} />
+        </View>
+
+        <View style={styles.login}>
+
+
+          <TextInput style={styles.textInput} onChangeText={(e) => { setName(e) }} value={hostName} placeholder={"Enter name"} placeholderTextColor={'#f9f9f9'}></TextInput>
+          <TouchableOpacity onPress={() => {
+
+            navigation.navigate('HostFilter');
+          }} style={styles.button}>
+            <LinearGradient
+              start={{ x: 0.0, y: 0.0 }}
+              end={{ x: 0.0, y: 0.0 }}
+              locations={[0.0, 0.74]}
+              colors={['#4ac6cd', '#49d695']}
+              style={styles.button}
+              useAngle={true}
+              angle={100}
+              angleCenter={{ x: 0.5, y: 0.5 }}
+            >
+              <Text style={styles.buttonText}>
+                HOST GAME
+          </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+
+
+          <Text style={styles.or}>OR</Text>
+
+
+          <TextInput style={styles.textInput} onChangeText={() => { }} value={hostName} placeholder={"Enter guest name"} placeholderTextColor={'#f9f9f9'}></TextInput>
+          <TextInput style={styles.textInput} onChangeText={() => { setCode }} placeholder={"Room Code"} placeholderTextColor={'#f9f9f9'}></TextInput>
+          <TouchableOpacity onPress={() => {
+
+            navigation.navigate('HostFilter');
+          }} style={styles.button}>
+            <LinearGradient
+              start={{ x: 0.0, y: 0.0 }}
+              end={{ x: 0.0, y: 0.0 }}
+              locations={[0.0, 0.74]}
+              colors={['#4ac6cd', '#49d695']}
+              style={styles.button}
+              useAngle={true}
+              angle={300}
+              angleCenter={{ x: 0.5, y: 0.5 }}
+            >
+              <Text style={styles.buttonText}>
+                JOIN GAME
+          </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+
+        </View>
+      </View>
+      <View style={styles.devs}>
+        <Text style={styles.dev}>Deveoloped by:</Text>
+        <Text style={styles.dev}>Nate Masters, Jonathan Bennett,</Text>
+        <Text style={styles.dev}>Hasan Alfridi, Peter Keenan,</Text>
+        <Text style={styles.dev}>Scott Oster</Text>
+      </View>
     </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
   loginPage: {
-    background: 'rgba(0, 0, 0, 0.3)',
-  paddingTop: '5px',
-  paddingBottom: '15px',
-  marginLeft: '10%',
-  marginRight: '10%',
-  marginTop: '10%',
-  borderRadius: '10px',
-  height: '50%',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    paddingTop: '5px',
+    paddingBottom: '15px',
+    marginLeft: '10%',
+    marginRight: '10%',
+    marginTop: '10%',
+    marginBottom: '10%',
+    borderRadius: '10px',
+    height: '80%',
   },
 
   body: {
     flex: 1,
-  }
- 
+  },
 
+  loginLogo: {
+    height: '100px',
+    width: '100px',
+    // maxWidth: '30%',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    paddingTop: '10px',
+    paddingBottom: '10px',
+    // width: '50%',
+    marginTop: '10%',
+    marginBottom: '50px',
+  },
+
+  login: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginLeft: '20%',
+    marginRight: '20%',
+  },
+
+  textInput: {
+    // opacity: '0.1',
+    marginBottom: '10px',
+    borderBottomColor: '#f9f9f9',
+    borderBottomWidth: '1px',
+    padding: '5px',
+    textAlign: 'left',
+
+  },
+
+  button: {
+    width: 120,
+    height: 40,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginBottom: '10px',
+    marginTop: '10px',
+    display: 'flex',
+    justifyContent: 'center',
+    borderRadius: 10,
+    // backgroundColor: '#2c3e50',
+  },
+
+  buttonText: {
+    fontSize: 14,
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+  },
+
+  or: {
+    color: '#f9f9f9',
+    textAlign: 'center',
+    margin: '10px',
+  },
+
+  devs: {
+    textAlign: 'center',
+    marginBottom: '2%',
+  },
+
+  dev: {
+    fontSize: 10,
+    color: '#f9f9f9',
+
+  }
 })
