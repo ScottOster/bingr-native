ad515a815898e27ff6b4e8ae5c334f1b5d7d8c98
Jonathan Bennett <bennettjonathan55@gmail.com>
2021-04-28 12:42:09 +0100

    added movie card styling

--- Components/MovieCard.js
+++ Components/MovieCard.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, Image, Button } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { LinearGradient } from 'expo-linear-gradient';
 import { updateVotesTally, getMovieByPosition, updateVotesCount } from '../firebase-api';
 
 export const MovieCard = ({ navigation, route }) => {
@@ -43,15 +45,21 @@ export const MovieCard = ({ navigation, route }) => {
   const { title, vote_average, overview, poster_path, id } = currentFilm;
 
   return (
-    <View>
-      <Text>{title}</Text>
-      <Text>{vote_average}</Text>
-      <Text>{overview}</Text>
+    <View style={styles.container}>
+      <View style={styles.backgroundContainer}>
       <Image
-        style={styles.tinyLogo}
+         style={styles.backgroundImage}
         source={{ uri: `https://image.tmdb.org/t/p/w500${poster_path}` }}
-      />
-      <Button
+        />
+      </View>
+      <LinearGradient colors={['transparent', '#000000']} style={styles.fullBackground}>
+      <View style = {styles.info}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.title}>Rating: {vote_average}</Text>
+      <Text style = {styles.description}>{overview}</Text>
+      <View style={styles.bothButtons}>
+      <TouchableOpacity
+      style={styles.button}
         title="cringr"
         disabled={disabledBtn}
         onPress={() => {
@@ -59,8 +67,21 @@ export const MovieCard = ({ navigation, route }) => {
           updateVotesTally(roomCode, String(id));
           setDisabledBtn(true);
         }}
-      />
-      <Button
+        >
+          <LinearGradient
+            start={{ x: 0.0, y: 0.0 }}
+            end={{ x: 0.0, y: 0.0 }}
+            locations={[0.0, 0.74]}
+            colors={['#ff5050', '#d4815d']}
+            style={styles.button}
+            useAngle={true}
+            angle={300}
+            angleCenter={{ x: 0.5, y: 0.5 }}>
+            <Text style={styles.buttonText}>Cringr</Text>
+          </LinearGradient>
+        </TouchableOpacity>
+      <TouchableOpacity
+      style={styles.button}
         title="bingr"
         disabled={disabledBtn}
         onPress={() => {
@@ -69,21 +90,84 @@ export const MovieCard = ({ navigation, route }) => {
           updateVotesCount(roomCode, String(id));
           setDisabledBtn(true);
         }}
-      />
-    </View>
+        >
+          <LinearGradient
+            start={{ x: 0.0, y: 0.0 }}
+            end={{ x: 0.0, y: 0.0 }}
+            locations={[0.0, 0.74]}
+            colors={['#4ac6cd', '#49d695']}
+            style={styles.button}
+            useAngle={true}
+            angle={300}
+            angleCenter={{ x: 0.5, y: 0.5 }}>
+            <Text style={styles.buttonText}>Bingr</Text>
+          </LinearGradient>
+        </TouchableOpacity>
+        </View>
+        </View>
+        </LinearGradient>
+  </View>
   );
 };
 
 const styles = StyleSheet.create({
+  fullBackground: {
+    flex: 1,
+  },
   container: {
-    paddingTop: 50,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
   },
-  tinyLogo: {
-    width: 50,
+  backgroundContainer: {
+    flex: 1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  },
+  backgroundImage: {
+    flex: 1,
+    width: null,
+    height: null,
+  },
+  info:{
+    flex: 1,
+  justifyContent: 'flex-end',
+  },
+  title:{
+    color: '#FFFFFF',
+    fontSize: 20,
+    margin: 10,
+    marginBottom: 0
+    
+  },
+  description:{
+    color: '#FFFFFF',
+    margin: 10,
+    marginBottom: 20
+  },
+  button: {
+    width: 140,
     height: 50,
+    marginBottom: 30,
+    marginTop: 20,
+    display: 'flex',
+    justifyContent: 'center',
+    borderRadius: 10,
+    // backgroundColor: '#2C3E50',
   },
-  logo: {
-    width: 66,
-    height: 58,
+  buttonText: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#FFFFFF',
   },
-});
+  bothButtons:{
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  }
+})
--- Components/MovieCardStyles.js
+++ Components/MovieCardStyles.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text, Image, Button } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export const MovieCardStyles = () => {
   return (
@@ -11,16 +12,38 @@ export const MovieCardStyles = () => {
           source={{ uri: 'https://image.tmdb.org/t/p/w500/npOnzAbLh6VOIu3naU5QaEcTepo.jpg' }}
         />
       </View>
-      <LinearGradient colors={['#C14242', '#ffffff']} style={styles.fullBackground}>
-        <View>
-          <Text>Castle in the Sky</Text>
-          <Text>7.9</Text>
-          <Text>
+      <LinearGradient colors={['transparent', '#000000']} style={styles.fullBackground}>
+        <View style = {styles.info}>
+          <Text style={styles.title}>Castle in the Sky</Text>
+          <Text style={styles.title}>7.9</Text>
+          <Text style = {styles.description}>
             A young boy and a girl with a magic crystal must race against pirates and foreign agents
             in a search for a legendary floating castle."
           </Text>
-          <Button title="cringr" />
-          <Button title="bingr" />
+          <View style={styles.bothButtons}>
+          <TouchableOpacity style={styles.button} title="cringr" ><LinearGradient
+            start={{ x: 0.0, y: 0.0 }}
+            end={{ x: 0.0, y: 0.0 }}
+            locations={[0.0, 0.74]}
+            colors={['#ff5050', '#d4815d']}
+            style={styles.button}
+            useAngle={true}
+            angle={300}
+            angleCenter={{ x: 0.5, y: 0.5 }}>
+            <Text style={styles.buttonText}>Cringr</Text>
+          </LinearGradient></TouchableOpacity>
+          <TouchableOpacity style={styles.button} title="bingr" ><LinearGradient
+            start={{ x: 0.0, y: 0.0 }}
+            end={{ x: 0.0, y: 0.0 }}
+            locations={[0.0, 0.74]}
+            colors={['#4ac6cd', '#49d695']}
+            style={styles.button}
+            useAngle={true}
+            angle={300}
+            angleCenter={{ x: 0.5, y: 0.5 }}>
+            <Text style={styles.buttonText}>Bingr</Text>
+          </LinearGradient></TouchableOpacity>
+          </View>
         </View>
       </LinearGradient>
     </View>
@@ -50,4 +73,41 @@ const styles = StyleSheet.create({
     width: null,
     height: null,
   },
-});
+  info:{
+    flex: 1,
+  justifyContent: 'flex-end',
+  },
+  title:{
+    color: '#FFFFFF',
+    fontSize: 20,
+    margin: 10,
+    marginBottom: 0
+    
+  },
+  description:{
+    color: '#FFFFFF',
+    margin: 10,
+    marginBottom: 20
+  },
+  button: {
+    width: 140,
+    height: 50,
+    marginBottom: 30,
+    marginTop: 20,
+    display: 'flex',
+    justifyContent: 'center',
+    borderRadius: 10,
+    // backgroundColor: '#2C3E50',
+  },
+  buttonText: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#FFFFFF',
+  },
+  bothButtons:{
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  }
+})
